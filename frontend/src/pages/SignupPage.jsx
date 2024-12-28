49f3470f7c61cf4656bc88e91c87f58e1d0b5a1c
Aayush-Pandey05 <aayushpandeydsce23@gmail.com>
2024-12-28 21:08:58 +0530

    Fixed changes again

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -4,7 +4,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import {Eye,EyeOff,Loader2,Lock,Mail,MessageSquare,User,} from "lucide-react";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
-import AuthImagePattern from "../components/AuthImagePattern";
+import AuthImagePattern from "../Components/AuthImagePattern";
 
 const SignupPage = () => {
   const [showPassword, setShowPassword] = useState(false);
